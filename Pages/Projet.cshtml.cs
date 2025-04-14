2575fa48470b329ba128255a6cc8e395e91d2bd8
Thierno Barry <tibarry10@outlook.com>
2025-04-14 16:14:44 +0200

    nouvelle version

---
+++ Pages/Projet.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebSite.Pages
+{
+    public class ProjetModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
